3b711a14cfd8c016c5d5049c507e3d5d472dd384
Jony-Mia <ajonymia321@gmail.com>
2026-01-04 18:31:40 +0600

    icon library

---
+++ script.js
@@ -0,0 +1,166 @@
+import icons from './js/fa.js';
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('fa')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('brands')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fas ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('solid')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fas ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('regular')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`far ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('light')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-light ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('thin')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-light ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharp')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp fas ${i}`
+    
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+    
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpRegular')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp far ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpLight')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp fas ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpThin')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp fa-thin ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+
+
+
+
+
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpDuotone')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp-duotone fas ${i}`
+    
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+    
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpDuotoneRegular')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp far ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpDuotoneThin')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp fas ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
+icons.forEach(i=>{
+    let iconPack= document.getElementById('sharpDuotoneLight')
+    let btn = document.createElement('button')
+    let it= document.createElement('i')
+    it.className=`fa-sharp fa-thin ${i}`
+
+    btn.appendChild(it)
+    iconPack.appendChild(btn)
+
+    console.log(i)
+})
